f87d01034fbb3ff1679c6f16cc88b80a96385428
Sam Darling <sam8ddd@gmail.com>
2024-01-13 19:01:20 -0700

    prep3 part 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicStr = System.Console.ReadLine();
+        int magic = int.Parse(magicStr);
+
+        System.Console.Write("What is your guess? ");
+        string guessStr = System.Console.ReadLine();
+        int guess = int.Parse(guessStr);
+
+        if (guess == magic)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (guess > magic)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            System.Console.WriteLine("Higher");
+        }
     }
 }
