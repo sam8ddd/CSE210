dd743aa85994af023f0e907fb9392521b9fb496e
Sam Darling <sam8ddd@gmail.com>
2024-01-15 13:25:31 -0700

    Prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("WHat is your favorite integer? ");
+            int faveNum = int.Parse(Console.ReadLine());
+            return faveNum;
+        }
+
+        static long SquareNumber(int faveNum)
+        {
+            return faveNum*faveNum;
+        }
+
+        static void DisplayResult(string name, long faveSquare)
+        {
+            Console.WriteLine($"{name}, the square of your favorite number is {faveSquare}");
+        }
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int faveNum = PromptUserNumber();
+        long faveSquare = SquareNumber(faveNum);
+        DisplayResult(name, faveSquare);
     }
 }
